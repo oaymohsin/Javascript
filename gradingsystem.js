03472c5898064019f323e7740984412ab6e5a377
oaymohsin <oaymohsin@gmail.com>
2022-07-02 11:22:37 -0700

    send

---
+++ gradingsystem.js
@@ -0,0 +1,60 @@
+
+let calculate=()=>{
+    let math,english,computer,urdu;
+math=document.getElementById("mathnbr").value;
+english=document.getElementById("engnbr").value;
+computer=document.getElementById("comnbr").value;
+urdu=document.getElementById("urdunbr").value;
+result=document.getElementById("resultarea");
+    let totalmarks=400;
+   
+
+    if(math>100 ||english>100 || computer>100 || urdu>100){
+        result.innerHTML="Numbers greater than 100 in any field are not allowed because total marks of every subject are 100";
+    }
+    else{
+        let percentage=((parseInt(math)+parseInt(english)+parseInt(computer)+parseInt(urdu))*100)/400;
+        if(percentage>85 && percentage<=100 ){
+            result.innerHTML=`You got ${percentage}% marks and A+ Grade`;
+        }
+        else if(percentage<=85 && percentage>=80){
+            result.innerHTML=`You got ${percentage}% marks and A Grade`;
+        }
+        else if(percentage<80 && percentage>=70){
+            result.innerHTML=`You got ${percentage}% marks and B Grade`;
+        }
+        else if(percentage<70 && percentage>=50){
+            result.innerHTML=`You got ${percentage}% marks and C Grade`;
+        }
+        else if(percentage<50 && percentage>=33){
+            result.innerHTML=`You got ${percentage}% marks and B Grade`;
+        }
+        else{
+            result.innerHTML=`You got ${percentage}% marks and you are failed`;
+        }
+    }
+    
+}
+function  reset (){
+    //  document.getElementById('mathnbr').value="";
+    document.getElementById("mathnbr").value="";
+    document.getElementById("comnbr").value="";
+    document.getElementById("urdunbr").value="";
+    document.getElementById("engnbr").value="";
+    document.getElementById("resultarea").innerHTML="";
+    // result.innerHTML=`reset is working`;
+}
+// let validate=()=>{
+//     let math,english,computer,urdu;
+// math=document.getElementById("mathnbr").value;
+// english=document.getElementById("engnbr").value;
+// computer=document.getElementById("comnbr").value;
+// urdu=document.getElementById("urdunbr").value;
+// if (math>100){
+//     document.getElementById("resultarea").innerHTML="*Number greater than 100 are not allowed";
+// }
+// else{
+//     document.getElementById("resultarea").innerHTML="";
+
+// }
+// }
